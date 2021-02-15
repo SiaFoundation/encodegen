8fdaf18ae689f5c16f6f975b05a6cc880f4a61be
Christopher Tarry <chrisjtarry@gmail.com>
2021-02-15 14:24:39 -0500

    use recursion to allow for structs within structs

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"go/parser"
 	"go/token"
 	"log"
+	"strings"
 )
 
 func main() {
@@ -25,9 +26,18 @@ type TestType1 struct {
 }
 type TestType2 struct {
 	I string
+	J TestType3
 }
 
-var a = 5;
+type TestType3 struct {
+	K uint64
+	L TestType4
+}
+
+type TestType4 struct {
+	K string
+}
+
 `
 	// Create the AST by parsing src.
 	fs := token.NewFileSet() // positions are relative to fset
@@ -61,24 +71,9 @@ var a = 5;
 	codeOutput := jen.NewFile("main")
 
 	for structName, structType := range structs {
-		var statements []jen.Code
 		unmarshalFunc := jen.Func().Params(jen.Id("r").Op("*").Id(structName)).Id("unmarshalBuffer").Params(jen.Id("b").Op("*").Id("objBuffer"))
-		for _, structField := range structType.Fields.List {
-			if len(structField.Names) > 0 {
-				field := getFieldType(structField.Type)
-				if field == nil {
-					continue
-				}
-				fieldAssignment := jen.Id("r").Dot(structField.Names[0].Name).Op("=")
-				if field.Primitive {
-					fieldAssignment = fieldAssignment.Op(field.Name).Parens(jen.Id("b").Dot(field.FunctionName).Call())
-				} else {
-					fieldAssignment = fieldAssignment.Parens(jen.Id("b").Dot(field.FunctionName).Call())
-				}
+		statements := getFieldAssignmentStatements(structs, []string{}, structName, structType)
 
-				statements = append(statements, fieldAssignment)
-			}
-		}
 		codeOutput.Add(unmarshalFunc.Block(statements...))
 	}
 	log.Printf("%#v", codeOutput)
@@ -90,6 +85,39 @@ type FieldType struct {
 	FunctionName string
 	Primitive    bool
 	Pointer      bool
+}
+
+func getFieldAssignmentStatements(structs map[string]*ast.StructType, prefixs []string, currentStructName string, currentStructType *ast.StructType) []jen.Code {
+	var statements []jen.Code
+	var fieldAssignment *jen.Statement
+	for _, structField := range currentStructType.Fields.List {
+		if len(structField.Names) > 0 {
+			field := getFieldType(structField.Type)
+			if field == nil {
+				continue
+			}
+
+			if len(prefixs) == 0 {
+				fieldAssignment = jen.Id("r").Dot(structField.Names[0].Name).Op("=")
+			} else {
+				fieldAssignment = jen.Id("r")
+				for _, prefix := range prefixs {
+					fieldAssignment = fieldAssignment.Dot(prefix)
+				}
+				fieldAssignment = fieldAssignment.Dot(structField.Names[0].Name).Op("=")
+			}
+
+			if field.Primitive {
+				fieldAssignment = fieldAssignment.Op(field.Name).Parens(jen.Id("b").Dot(field.FunctionName).Call())
+				statements = append(statements, fieldAssignment)
+			} else {
+				prefixs = append(prefixs, structField.Names[0].Name)
+				statements = append(statements, getFieldAssignmentStatements(structs, prefixs, field.Name, structs[field.Name])...)
+			}
+
+		}
+	}
+	return statements
 }
 
 //Returns a string representation of the given expression if it was recognized.
@@ -105,6 +133,13 @@ func getFieldType(exp ast.Expr) *FieldType {
 				Pointer:      false,
 			}
 			log.Print("primitive")
+		} else {
+			return &FieldType{
+				Name:         v.Name,
+				FunctionName: "read" + strings.Title(v.Name),
+				Primitive:    false,
+				Pointer:      false,
+			}
 		}
 		break
 		// case *ast.ArrayType:
