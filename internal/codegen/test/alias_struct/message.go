fb2b9531dac2073a16f01e61b613c093ef4b4a5e
Christopher Tarry <chrisjtarry@gmail.com>
2021-03-04 09:15:39 -0500

    add folder for alias test that was accidentally excluded

---
+++ internal/codegen/test/alias_struct/message.go
@@ -0,0 +1,30 @@
+package alias_struct
+
+// import 	"gitlab.com/NebulousLabs/Sia/crypto"
+
+type DoubleAliasSubMessage AliasSubMessage
+type AliasSubMessage SubMessage
+
+type AliasInt int
+type DoubleAliasInt AliasInt
+type AliasIntArray []int
+type AliasIntPointerArray []*int
+
+type AliasSubMessageArray []SubMessage
+type AliasSubMessagePointerArray []*SubMessage
+
+type Message struct {
+	Id                                int
+	Sub                               SubMessage
+	AliasSubMessageField              AliasSubMessage
+	ArrayAliasSubMessageField         []AliasSubMessage
+	DoubleAliasSubMessageField        DoubleAliasSubMessage
+	PointerDoubleAliasSubMessageField *DoubleAliasSubMessage
+	AliasIntField                     AliasInt
+	PointerAliasIntField              *AliasInt
+	AliasIntArrayField                AliasIntArray
+	AliasIntPointerArrayField         AliasIntPointerArray
+	AliasSubMessageArrayField         AliasSubMessageArray
+	AliasSubMessagePointerArrayField  AliasSubMessagePointerArray
+	DoubleAliasIntField               DoubleAliasInt
+}
---
+++ internal/codegen/test/alias_struct/sub_message.go
@@ -0,0 +1,7 @@
+package alias_struct
+
+type SubMessage struct {
+	Id          int
+	Description string
+	Strings     []string
+}
